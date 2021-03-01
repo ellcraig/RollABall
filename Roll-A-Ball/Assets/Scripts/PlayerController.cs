62f0152c76a33055e3dcbc240b7121b03e8d9898
Elliot Craig <ellcraig@iu.edu>
2021-03-01 17:27:52 -0500

    Completed Tutorial

--- Roll-A-Ball/Assets/Scripts/PlayerController.cs
+++ Roll-A-Ball/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
     public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
     private Rigidbody rb;
     private float movementX;
     private float movementY;
@@ -20,6 +22,7 @@ public class PlayerController : MonoBehaviour
         count = 0;
 
         SetCountText();
+        winTextObject.SetActive(false);
     }
     void OnMove(InputValue movementValue)
     {
@@ -32,6 +35,10 @@ public class PlayerController : MonoBehaviour
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
+        if(count >= 12)
+        {
+            winTextObject.SetActive(true);
+        }
     }
 
     void FixedUpdate()
