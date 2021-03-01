729ad683f5711372841d2cf27db99b02a0eb0ad1
Elliot Craig <ellcraig@iu.edu>
2021-03-01 17:17:04 -0500

    Continued development

--- Roll-A-Ball/Assets/Scripts/PlayerController.cs
+++ Roll-A-Ball/Assets/Scripts/PlayerController.cs
@@ -2,19 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
-
+    public TextMeshProUGUI countText;
     private Rigidbody rb;
     private float movementX;
     private float movementY;
+    private int count;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
     }
     void OnMove(InputValue movementValue)
     {
@@ -24,6 +29,11 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y;
     }
   
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
+
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
@@ -36,6 +46,9 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("PickUp"))
         {
         other.gameObject.SetActive(false);
+        count = count + 1;
+
+        SetCountText();
         }
     }
 }
